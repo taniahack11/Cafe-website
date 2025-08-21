ec0a2f94e810085a6ea4b988e0f5ceac42d2134e
Tania Islam <tania.islam2906@gmail.com>
2025-08-21 21:42:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector('#menuimg').addEventListener("click",()=>{
+    alert("Check out our menu!");
+});
+
+document.querySelector('#loginimg').addEventListener("click",()=>{
+    alert("Login to place your order.");
+});
+
+document.querySelector('#feedbackimg').addEventListener("click",()=>{
+    alert("We would love to have your feedback!.");
+});
+
+document.querySelector('#menulink').addEventListener("click",()=>{
+    alert("Your perfect brew awaits you!");
+});
+
+document.querySelector('#loginlink').addEventListener("click",()=>{
+    alert("Taking you to the login page.");
+});
+
+document.querySelector('#feedbacklink').addEventListener("click",()=>{
+    alert("Fetching the feedback form for you.");
+});
